e5c2a6f590b108a626b1e7cd537d11371eb939ad
Victor Santos <v.santos@itsoluclick.com>
2025-01-03 16:12:05 -0500

    fixing build

--- src/app/(guest-only)/forgot-password/page.tsx
+++ src/app/(guest-only)/forgot-password/page.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from 'next'
 
 // Component Imports
-import ForgotPassword from '@views/ForgotPassword'
+import ForgotPassword from '@views/pages/auth/ForgotPassword'
 
 // Server Action Imports
 import { getServerMode } from '@core/utils/serverHelpers'
